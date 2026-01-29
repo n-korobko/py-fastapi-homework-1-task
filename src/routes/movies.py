c625135511b6271d4c254cb7b3663a88da708023
Nikita <hkorobko@gmail.com>
2026-01-29 17:22:54 +0200

    Added schemas and routes

--- src/routes/movies.py
+++ src/routes/movies.py
@@ -1,10 +1,71 @@
+import math
+
 from fastapi import APIRouter, Depends, HTTPException, Query
-from sqlalchemy import select
+from sqlalchemy import select, func
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from database import get_db, MovieModel
-
+from database import get_db
+from database.models import MovieModel
+from schemas.movies import MovieDetailResponseSchema, MovieListResponseSchema
 
 router = APIRouter()
 
-# Write your code here
+
+@router.get("/movies/", response_model=MovieListResponseSchema)
+async def get_movies(
+    page: int = Query(1, ge=1),
+    per_page: int = Query(10, ge=1, le=20),
+    db: AsyncSession = Depends(get_db),
+):
+    total_items = await db.scalar(select(func.count()).select_from(MovieModel))
+    total_items = int(total_items or 0)
+
+    if total_items == 0:
+        raise HTTPException(status_code=404, detail="No movies found.")
+
+    total_pages = math.ceil(total_items / per_page)
+    offset = (page - 1) * per_page
+
+    result = await db.execute(
+        select(MovieModel)
+        .order_by(MovieModel.id)
+        .offset(offset)
+        .limit(per_page)
+    )
+    movies = result.scalars().all()
+
+    if not movies:
+        raise HTTPException(status_code=404, detail="No movies found.")
+
+    prev_page = (
+        f"/api/v1/theater/movies/?page={page - 1}&per_page={per_page}"
+        if page > 1
+        else None
+    )
+    next_page = (
+        f"/api/v1/theater/movies/?page={page + 1}&per_page={per_page}"
+        if page < total_pages
+        else None
+    )
+
+    return {
+        "movies": movies,
+        "prev_page": prev_page,
+        "next_page": next_page,
+        "total_pages": total_pages,
+        "total_items": total_items,
+    }
+
+
+@router.get("/movies/{movie_id}/", response_model=MovieDetailResponseSchema)
+async def get_movie(movie_id: int, db: AsyncSession = Depends(get_db)):
+    movie = await db.get(MovieModel, movie_id)
+
+    if movie is None:
+        raise HTTPException(
+            status_code=404,
+            detail="Movie with the given ID was not found.",
+        )
+
+    return movie
+
